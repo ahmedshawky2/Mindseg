63ab6c255b644b7783a636aa0387e838777c4edc
ahmedshawky2 <46525840+ahmedshawky2@users.noreply.github.com>
2019-07-30 22:46:39 +0200

    Add files via upload

---
+++ MindsLineItems/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MindsLineItems',
+    'version': '12.0.1.0',
+    
+    'description': u"""
+access LineItems 
+""",
+    'author': 'MindsEg',
+    'depends': [
+        'account',
+        'account_accountant',
+    ],
+    'data': [
+        'data/ir_ui_view.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_menu.xml',
+        
+    ],
+    'application': False,
+    'license': 'OPL-1',
+}
