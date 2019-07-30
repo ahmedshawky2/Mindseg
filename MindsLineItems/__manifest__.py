b8eb30b571b8b0d936c53713a888ac6566c0bc44
ahmedshawky2 <46525840+ahmedshawky2@users.noreply.github.com>
2019-07-30 23:27:49 +0200

    Add files via upload

--- MindsLineItems/__manifest__.py
+++ MindsLineItems/__manifest__.py
@@ -1,19 +1,22 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'MindsLineItems',
-    'description': 'MindsLineItems',
+    'version': '12.0.1.0',
+    'category': 'Studio',
+    'description': u"""
+Access Invocie Line Item
+""",
     'author': 'Minds',
-    'depends': ['base','account','account_accountant'],
-    'application': True,
+    'depends': [
+        'account',
+        'account_accountant',
+    ],
     'data': [
-
-           'data/ir_ui_view.xml',
+        'data/ir_ui_view.xml',
         'data/ir_actions_act_window.xml',
         'data/ir_ui_menu.xml',
-        
-
-
-
-
-        ],
-    'installable':True
+       
+    ],
+    'application': False,
+    'license': 'OPL-1',
 }
